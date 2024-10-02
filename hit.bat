2c531ca5473d571a1aece3224d7fc421927d28c5
AVADESH KUMAR PAL <89032572+Avadeshpal12@users.noreply.github.com>
2024-10-02 23:43:55 +0530

    Add files via upload

---
+++ hit.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\WinSCP\WinSCP.exe" /script="C:\Users\AKS Topside Product\Desktop\local2remote.txt"
+DEL /Q "C:\Users\AKS Topside Product\Downloads"
---
+++ hit_VBS.vbs
@@ -0,0 +1,3 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run chr(34) & "C:\Users\AKS Topside Product\Desktop\hit.bat" & Chr(34), O
+Set WshShell = Nothing
